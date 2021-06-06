901496cb2df210cd4594d293c09b98e620afb01a
AhmedSayedabed <77204441+AhmedSayedabed@users.noreply.github.com>
2021-06-06 16:54:44 +0200

    Update main0.c
    Port F Configuration

--- main0.c
+++ main0.c
@@ -10,6 +10,19 @@ void SystemInit(){}
 #define BLUE 0x04
 #define GREEN 0x08
 	
+void init_F(void){
+      uint32_t delay ;
+      SYSCTL_RCGCGPIO_R |= 0x00000020 ;
+      delay = 1;
+		  GPIO_PORTF_LOCK_R =0x4c4f434B;
+	    GPIO_PORTF_CR_R =0x1F;
+      GPIO_PORTF_AFSEL_R = 0 ;
+      GPIO_PORTF_AMSEL_R = 0 ;
+      GPIO_PORTF_DEN_R = 0x1F ;
+      GPIO_PORTF_DIR_R = 0x0E ;
+      GPIO_PORTF_PCTL_R = 0 ;   
+      GPIO_PORTF_PUR_R =0x11;
+}
 	void delay(void){
   unsigned long volatile time;
   time = 145448;  // 0.1sec

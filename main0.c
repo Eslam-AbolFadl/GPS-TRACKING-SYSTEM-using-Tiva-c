4b60fa793acec695cc9532ef58761b9cd0bd2d75
AhmedSayedabed <77204441+AhmedSayedabed@users.noreply.github.com>
2021-06-06 16:59:52 +0200

    Update main0.c

--- main0.c
+++ main0.c
@@ -29,5 +29,35 @@ void init_F(void){
   while(time){
  		time--;
   }
+void LCD_init(void){
+      uint32_t delay ;
+      SYSCTL_RCGCGPIO_R |= 0x00000003 ; //for Port A and Port B
+      delay = 1;
+          GPIO_PORTB_LOCK_R =0x4c4f434B;
+        GPIO_PORTB_CR_R =0xFF;
+      GPIO_PORTB_AFSEL_R = 0 ;
+      GPIO_PORTB_AMSEL_R = 0 ;
+      GPIO_PORTB_DEN_R = 0xFF ;// we need all bits for lcd output
+      GPIO_PORTB_DIR_R = 0xFF ;
+      GPIO_PORTB_PCTL_R = 0 ;   
+      GPIO_PORTB_PUR_R =0x11;
+    
+      delay = 1;
+          GPIO_PORTA_LOCK_R =0x4c4f434B;
+        GPIO_PORTA_CR_R =0xFF;
+      GPIO_PORTA_AFSEL_R = 0 ;
+      GPIO_PORTA_AMSEL_R = 0 ;
+      GPIO_PORTA_DEN_R = 0xE0 ; // we only need the last 3 bits for control
+      GPIO_PORTA_DIR_R = 0xE0 ;
+      GPIO_PORTA_PCTL_R = 0 ;   
+      GPIO_PORTA_PUR_R =0xE0; 
+            
+            LCD_Command(0x30); //wakeup
+            LCD_Command(0x38); //to make the lcd work on 8 bits
+            LCD_Command(0x06); //increment crussor to the right
+            LCD_Command(0x01); //to clear the screen
+            LCD_Command(0x0F); //to turn the screen on
+            
+}
 }
  
